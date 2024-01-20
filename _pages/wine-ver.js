716df75fa954224e0498a1c67bea00020f8a0ed6
zetaepyon <zetaepyon@users.noreply.github.com>
2024-01-20 15:35:36 -0600

    Create wine-ver.js

---
+++ _pages/wine-ver.js
@@ -0,0 +1,12 @@
+fetch('https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/latest', {headers: {accept:'application/vnd.github+json',redirect: "follow",referrerPolicy: "no-referrer"}}).then(
+  res => {
+    if (res.ok) return res.json();
+    throw new Error();
+  }).then(
+  res => {
+    var match = res.name.match(/(.*) Released$/i);
+    document.getElementById("latestrelease").innerHTML = match && match.at(1) || res.name;
+  }
+).catch(err => {
+  document.getElementById("latestrelease").innerHTML = 'Unknown';
+});
